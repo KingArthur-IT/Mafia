31036799cfcfb0103132255650a3e4785696ec85
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-11-26 00:29:30 +0300

    table created

--- client/src/components/Profile/MainHero.vue
+++ client/src/components/Profile/MainHero.vue
@@ -3,9 +3,9 @@
       Рейтинг
       Уровень авторитета
       <Achivements />
-      <button class="btn hero__btn" @click="goToPage('profile.holl')">Играть</button>
-      <button class="btn hero__btn" @click="goToPage('profile.rules')">Правила</button>
-      <button class="btn hero__btn" @click="goToPage('profile.statistics')">Статистика</button>
+      <button class="btn secondary-btn hero__btn" @click="goToPage('profile.holl')">Играть</button>
+      <button class="btn secondary-btn hero__btn" @click="goToPage('profile.rules')">Правила</button>
+      <button class="btn secondary-btn hero__btn" @click="goToPage('profile.statistics')">Статистика</button>
   </div>
   
 </template>
@@ -31,17 +31,7 @@ export default {
             margin-bottom: 12px
             font-weight: 700
         &__btn
-            display: block
-            background: transparent
-            border: 1px solid var(--color-primary)
-            font-weight: 600
-            border-radius: 5px
             padding: 12px 30px
-            transition: color .2s ease, background-color .2s ease
             min-width: 250px
             margin-bottom: 12px
-            &:hover
-                background-color: var(--color-primary)
-            &::active  
-                background-color: transparent
 </style>
--- client/src/views/HollView.vue
+++ client/src/views/HollView.vue
@@ -1,13 +1,55 @@
 <template>
-  
+    <div class="holl">
+        <div class="holl__header">
+            <button class="btn secondary-btn holl__add-btn">
+                Добавить комнату
+                <PlusIcon class="holl__add-icon" />
+            </button>
+        </div>
+        <table class="table table-no-spacing">
+            <tr class="table__row">
+                <th width="50%">Название комнаты</th>
+                <th width="25%">Количество игроков</th>
+                <th width="25%">Доступные роли</th>
+            </tr>
+            <tr>
+                <td>Крутая комната</td>
+                <td>5/10</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Крутая комната</td>
+                <td>5/10</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
-export default {
+import PlusIcon from '@/components/icons/PlusIcon.vue'
 
+export default {
+    components:{
+        PlusIcon
+    }
 }
 </script>
 
-<style>
-
+<style scoped lang="sass">
+    .holl
+        &__header
+            width: 100%
+            display: flex
+            justify-content: flex-end
+            margin-bottom: 24px
+        &__add-btn
+            display: flex
+            justify-content: center
+            align-items: center
+            padding: 12px 30px
+        &__add-icon
+            margin-left: 12px
+            width: 14px
+            height: 14px
 </style>
