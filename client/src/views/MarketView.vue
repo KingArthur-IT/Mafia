4c0dbc8a78054a6d1760d4d0e91363a2ecac82b8
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-11-26 20:23:55 +0300

    edits

--- client/src/views/MarketView.vue
+++ client/src/views/MarketView.vue
@@ -1,13 +1,67 @@
 <template>
-  
+  <div class="market">
+      <div class="market__wrap">
+          <div class="market__item">
+              <h2 class="market__title">Базовый аккаунт</h2>
+              <ul class="market__list">
+                  <li class="market__line">что-то</li>
+              </ul>
+          </div>
+      </div>
+      <div class="market__wrap">
+          <div class="market__item">
+              <h2 class="market__title">VIP аккаунт <CrownIcon /></h2>
+              <ul class="market__list">
+                  <li class="market__line">+20% к игровому рейтингу</li>
+                  <li class="market__line">возможность выбирать игровую роль</li>
+              </ul>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
-export default {
+import CrownIcon from '@/components/icons/CrownIcon.vue'
 
+export default {
+    components:{
+        CrownIcon
+    }
 }
 </script>
 
-<style>
+<style scoped lang="sass">
+    .market
+        width: 100%
+        display: flex
+        align-items: stretch
+        &__wrap
+            flex-basis: 50%
+            padding: 0 12px
+        &__item
+            border-radius: 5px
+            border: 1px solid #fff
+            padding: 12px
+            height: 100%
+        &__title
+            text-align: center
+            margin-bottom: 24px
+            display: flex
+            align-items: center
+            justify-content: center
+            & svg
+                width: 40px
+                margin-left: 12px
+        &__line
+            padding-left: 20px
+            position: relative
+            &::before
+                content: ''
+                position: absolute
+                top: 9px
+                left: 2px
+                width: 8px
+                height: 8px
+                background: var(--color-primary)
 
 </style>
--- client/src/views/StatisticsView.vue
+++ client/src/views/StatisticsView.vue
@@ -1,13 +1,66 @@
 <template>
-  
+  <div class="stats">
+      <table class="table table-no-spacing">
+          <tr class="table__row">
+              <th width="60%"></th>
+              <th></th>
+          </tr>
+          <tr v-for="(item, i) in Object.keys(data)" :key="i" class="row">
+              <td>{{data[item].description}}</td>
+              <td>{{data[item].value}}</td>
+          </tr>
+      </table>
+  </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            data: {
+                allGames: {
+                    description: 'Сыгнано игр',
+                    value: 50
+                },
+                winsGames: {
+                    description: 'Побед',
+                    value: 50
+                },
+                wasMafia:{
+                    description: 'Был мафией',
+                    value: 50
+                },
+                wasSheriff:{
+                    description: 'Был шерифом',
+                    value: 50
+                },
+                wasDoctor:{
+                    description: 'Был доктором',
+                    value: 50
+                },
+                wasLover:{
+                    description: 'Был любовницей',
+                    value: 50
+                },
+                wasTerrorist:{
+                    description: 'Был террористом',
+                    value: 50
+                },
+                wasBarmen:{
+                    description: 'Был барменом',
+                    value: 50
+                },
+                wasBodyguard:{
+                    description: 'Был телохранителем',
+                    value: 50
+                },
+            }
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped lang="sass">
+    .row
+        cursor: default
 </style>
